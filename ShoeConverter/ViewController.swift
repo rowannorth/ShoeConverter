6155a704e095c7055c55306e82a1d25487183c6e
rowannorth <rowanowlnorth@gmail.com>
2014-09-24 22:11:12 -0700

    added labels, text field and button for shoe conversion.

--- ShoeConverter/ViewController.swift
+++ ShoeConverter/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    
+    @IBOutlet weak var mensShoeSizeTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
