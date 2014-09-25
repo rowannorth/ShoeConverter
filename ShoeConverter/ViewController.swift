4ca549f091e800e5236759efb97fe5bd9e41405c
rowannorth <rowanowlnorth@gmail.com>
2014-09-25 11:27:46 -0700

    Added women's shoe conversion and configured button with functionality to convert input to women's European shoe size.

--- ShoeConverter/ViewController.swift
+++ ShoeConverter/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var mensShoeSizeTextField: UITextField!
     @IBOutlet weak var mensConvertedShoeSizeLabel: UILabel!
+    @IBOutlet weak var womensShoeSizeTextField: UITextField!
+    @IBOutlet weak var womensConvertedShoeSizeLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -35,6 +37,15 @@ class ViewController: UIViewController {
         mensConvertedShoeSizeLabel.text = stringWithUpdatedShoeSize
     }
     
-
+    @IBAction func convertWomensShoeSizeButtonPressed(sender: UIButton) {
+        
+        let sizeFromTextField = Double((womensShoeSizeTextField.text as NSString).doubleValue)
+        let conversionConstant = 30.5
+        womensConvertedShoeSizeLabel.hidden = false
+        womensConvertedShoeSizeLabel.text = "\(sizeFromTextField + conversionConstant)" + " in European Shoe Size"
+        
+    }
+   
+    
 }
 
